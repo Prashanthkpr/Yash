c6ebb566618685be2c694ab867d17eed93eba9e8
azurejobways2021 <azurejobways2021@gmail.com>
2022-04-07 07:58:39 +0000

    Updated settings.py

--- TeamWork/settings.py
+++ TeamWork/settings.py
@@ -134,12 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    # '/var/www/static/',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"static"), "templates"]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
